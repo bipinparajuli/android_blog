118cc5c81665025ce0c614bfb173fb46b85a8e91
bipinparajuli <bipinprjl@gmail.com>
2022-02-22 18:36:55 +0545

    bug fix

--- app/src/main/java/com/example/travelblog/BlogDetails.java
+++ app/src/main/java/com/example/travelblog/BlogDetails.java
@@ -13,12 +13,12 @@ import com.bumptech.glide.Glide;
 
 public class BlogDetails extends AppCompatActivity {
 
-//    public static final String IMAGE_URL =
-//            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-//                    "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg";
-//    public static final String AVATAR_URL =
-//            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-//                    "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg";
+    public static final String IMAGE_URL =
+            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+                    "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg";
+    public static final String AVATAR_URL =
+            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+                    "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,13 +26,13 @@ public class BlogDetails extends AppCompatActivity {
         setContentView(R.layout.activity_blog_details);
 
         ImageView imageMain = findViewById(R.id.imageMain);
-        imageMain.setImageResource(R.drawable.sydney_image);
-//        Glide.with(this).load(IMAGE_URL).into(imageMain);
+//        imageMain.setImageResource(R.drawable.sydney_image);
+        Glide.with(this).load(IMAGE_URL).into(imageMain);
 
 
         ImageView imageAvatar = findViewById(R.id.imageAvatar);
-        imageAvatar.setImageResource(R.drawable.avatar);
-//        Glide.with(this).load(AVATAR_URL).into(imageAvatar);
+//        imageAvatar.setImageResource(R.drawable.avatar);
+        Glide.with(this).load(AVATAR_URL).into(imageAvatar);
 
 
         TextView textTitle = findViewById(R.id.textTitle);
